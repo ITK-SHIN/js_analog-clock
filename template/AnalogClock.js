8de512f2e8b97fe2a53df74a0e38d8bf89c163a8
신상우 <bano112@naver.com>
2023-05-30 16:37:36 +0900

    '현재시간작동확인'

--- template/AnalogClock.js
+++ template/AnalogClock.js
@@ -2,23 +2,92 @@ const AnalogClock = ($container) => {
   const analogClockBody = document.querySelector(".analog-clock");
   const div = document.createElement('div');
   
-  /* html element 동적 생성하기 시작 */
+  /********************************  html element 동적 생성하기 시작 ***********************/
   for (let i = 1; i <= 3; i++){
-    analogClockBody.insertAdjacentHTML('beforeend', `<div class='hand' ></div>`)
+    analogClockBody.insertAdjacentHTML('beforeend', `<div class='hand' ></div>`);
   }
-
-  const handdleClock =document.querySelectorAll('.hand')
+  const handdleClock =document.querySelectorAll('.hand');
   handdleClock[0].className += ' hour';
   handdleClock[1].className += ' minute';
   handdleClock[2].className += ' second';
 
-
   for (let i = 1; i <= 12; i++){
-    analogClockBody.insertAdjacentHTML('beforeend', `<div class="time time${i}">|</div>`)
+    analogClockBody.insertAdjacentHTML('beforeend', `<div class="time time${i}">|</div>`);
   }
-  /*html element 동적 생성하기 끝  */
+  /************************  html element 동적 생성하기 끝  ***********************************/
 
+  /*********************  자바스크립트로 --deg 값 변경하기 시작 *************************/
+  document.addEventListener("DOMContentLoaded", setInterval(() => {
+
+    //현재 시간 가져오기
+    const now = new Date();
+
+    //현재 시간을 단위로 추출
+    const seconds = now.getSeconds();  // 0 ~ 59 사이 정수 반환
+    const minutes = now.getMinutes();  // 0 ~ 59 사이 정수 반환
+    const hours = now.getHours();  // 0 ~ 23 사이 정수반환
+
+    //초침 , 분침 , 시침  -> 차례대로 각도
+    //초침 -> 1초당 몇도 회전
+    const degSec = seconds * (360 / 60);   // ex) 5초 -> 5 * 6 = 30도 
+
+    //분침 -> 1분당 몇도, 1초당 몇도회전
+    const degMin = minutes * (360 / 60) + seconds * (360 / 60 / 60);  // ex) 5분 -> 5 * 6 = 30도
+
+    //시침 -> 1시간당 30도, 1분당 몇도회전
+    // ex) 4시 -> ex) 4 * 30 = 120도
+    //ex2) 4시 15분 -> 4 *30 + 15 * 1/2
+    const degHour = hours * (360 / 12) + minutes * (360 / 12 / 60);
+   
+
+
+    const secondHand = document.querySelectorAll('.hand')[2];
+    const minuteHand = document.querySelectorAll('.hand')[1];
+    const hourHand = document.querySelectorAll('.hand')[0];
+
+    secondHand.style.setProperty('--deg', `${degSec}`);
+    minuteHand.style.setProperty('--deg', `${degMin}`);
+    hourHand.style.setProperty('--deg', `${degHour}`);
+  }, 1000));
+
+
+  /*********************  자바스크립트로 --deg 값 변경하기 끝 *************************/
+
+  /***************  여기서부터는 시도햇던 것들,, 제출전 삭제예정 **********************/
   
+/* //   setInterval(secondMove, 1000);
+//  setInterval(minuteMove, 1000);
+//  setInterval(hourMove, 1000); 
+  
+    let seconds = 0;
+    let minutes = 360;
+    let hours = 30;
+    let deg = 6;
+  
+  function secondMove() {
+    const secondElement = document.querySelectorAll('.hand')[2];
+    seconds++;
+    secondElement.style.setProperty('--deg', seconds * deg);
+     
+      console.log(secondElement);
+  }
+
+    function minuteMove() {
+      const minuteElement = document.querySelectorAll('.hand')[1];
+ 
+      minuteElement.style.setProperty('--deg', (minutes / 60)/60);
+      deg += deg / 60;
+
+      console.log(minuteElement);
+    }
+  
+      function hourMove() {
+       const hourElement = document.querySelectorAll('.hand')[0];
+        console.log(hourElement);
+        hourElement.style.setProperty('--deg', (hours / 60) / 60);
+        deg+= (hours / 60) / 60;
+  } */
+
 };
 
 export default AnalogClock;
