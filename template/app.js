1fd31428a6daad21d2ed2cc95356d1e91cd8fb98
신상우 <bano112@naver.com>
2023-05-31 16:10:42 +0900

    '정상작동gpt도움'

--- template/app.js
+++ template/app.js
@@ -1,3 +1,11 @@
 import AnalogClock from './AnalogClock.js';
 
-document.querySelectorAll('.analog-clock').forEach(AnalogClock);
+/* document.querySelectorAll('.analog-clock').forEach(AnalogClock); */
+
+/*   document.querySelectorAll('.analog-clock') */
+/* 
+AnalogClock();  */
+
+
+ const clocks = document.querySelectorAll('.analog-clock');
+    clocks.forEach(clock => new AnalogClock(clock));
