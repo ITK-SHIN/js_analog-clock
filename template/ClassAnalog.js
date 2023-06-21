dae8aa982cae19073264d841386f19f4b8cf2a93
신상우 <bano112@naver.com>
2023-06-21 22:54:40 +0900

    '최종ver'

--- template/ClassAnalog.js
+++ template/ClassAnalog.js
@@ -1,4 +1,5 @@
-class AnalogClock {
+/* class로 바꿔본 코드 */
+/* class AnalogClock {
   constructor($container) {
     this.$container = $container;
     this.createClockElements();
@@ -40,3 +41,4 @@ class AnalogClock {
 }
 
 export default AnalogClock;
+ */
--- template/app.js
+++ template/app.js
@@ -1,9 +1,12 @@
-/* import AnalogClock from './AnalogClock.js'; */
-import AnalogClock from './ClassAnalog.js';
+ import AnalogClock from './AnalogClock.js'; 
 
-/* document.querySelectorAll('.analog-clock').forEach(AnalogClock); */
+ document.querySelectorAll('.analog-clock').forEach(AnalogClock); 
+
+
+
+ /* class로 바꿔본 코드 */
+/* import AnalogClock from './ClassAnalog.js';
 const analogClocks = document.querySelectorAll('.analog-clock');
-
 analogClocks.forEach($container => {
          new AnalogClock($container)
-})
+}) */
