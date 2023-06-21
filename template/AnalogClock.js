9dff8e2f789bc52384e503ad45d8cf1b409fe844
신상우 <bano112@naver.com>
2023-06-21 21:55:58 +0900

    '리팩토링완료-아래주석설명'

--- template/AnalogClock.js
+++ template/AnalogClock.js
@@ -2,9 +2,10 @@ const AnalogClock = ($container) => {
   /********************************  html element 동적 생성하기 시작 ***********************/
 
   const handClasses = ['hour', 'minute', 'second']
-  for (let i = 0; i < handClasses.length; i++){
-    $container.insertAdjacentHTML('beforeend', `<div class='hand ${handClasses[i]}' ></div>`);
-  }
+  handClasses.forEach(handleClass => {
+  $container.insertAdjacentHTML('beforeend', `<div class='hand ${handClasses}' ></div>`);
+})
+
   const handdleClock =$container.querySelectorAll('.hand');
   handdleClock[0].className += ' hour';
   handdleClock[1].className += ' minute';
@@ -16,32 +17,34 @@ const AnalogClock = ($container) => {
   /************************  html element 동적 생성하기 끝  ***********************************/
 
   /*********************  자바스크립트로 --deg 값 변경하기 시작 *************************/
- setInterval(() => {
- 
-    //현재 시간 가져오기
-    const now = new Date();
+function setClock() {
+  const now = new Date();
+  const seconds = now.getSeconds();  // 0 ~ 59 사이 정수 반환
+  const minutes = now.getMinutes(); // 0 ~ 59 사이 정수 반환
+  const hours = now.getHours();  // 0 ~ 23 사이 정수반환
 
-    //현재 시간을 단위로 추출
-    const seconds = now.getSeconds();  // 0 ~ 59 사이 정수 반환
-    const minutes = now.getMinutes();  // 0 ~ 59 사이 정수 반환
-    const hours = now.getHours();  // 0 ~ 23 사이 정수반환
-
-    //초침 , 분침 , 시침  -> 차례대로 각도
-    //초침 -> 1초당 몇도 회전
-    const degSec = seconds * (360 / 60);   // ex) 5초 -> 5 * 6 = 30도 
-
-    //분침 -> 1분당 몇도, 1초당 몇도회전
-    const degMin = minutes * (360 / 60) + seconds * (360 / 60 / 60);  // ex) 5분 -> 5 * 6 = 30도
-
-    //시침 -> 1시간당 30도, 1분당 몇도회전
+   //초침 , 분침 , 시침  -> 차례대로 각도
+  //초침 -> 1초당 몇도 회전
+  const degSec = seconds * (360 / 60);   //ex) 5초 -> 5 * 6 = 30도 
+  const degMin = minutes * (360 / 60) + seconds * (360 / 60 / 60);  // ex) 5분 -> 5 * 6 = 30도
+  const degHour = hours * (360 / 12) + minutes * (360 / 12 / 60);
+   //시침 -> 1시간당 30도, 1분당 몇도회전
     // ex) 4시 -> ex) 4 * 30 = 120도
     //ex2) 4시 15분 -> 4 *30 + 15 * 1/2
-    const degHour = hours * (360 / 12) + minutes * (360 / 12 / 60);
+  handdleClock[2].style.setProperty('--deg', `${degSec}`);
+  handdleClock[1].style.setProperty('--deg', `${degMin}`);
+  handdleClock[0].style.setProperty('--deg', `${degHour}`);
+}
 
-    handdleClock[2].style.setProperty('--deg', `${degSec}`);
-    handdleClock[1].style.setProperty('--deg', `${degMin}`);
-    handdleClock[0].style.setProperty('--deg', `${degHour}`);
-  }, 1000);
+  setClock();
+  setInterval(setClock, 1000);
 };
 
+
+
+
 export default AnalogClock;
+
+//첫 번째 for  forEach 메소드를 사용하여 간단하게 리팩토링
+//setInterval 함수 내부의 코드를 setClock 함수로 분리하여 중복을 제거
+//setInterval 함수가 호출될 때마다 setClock 함수가 실행되어 시계의 초침, 분침, 시침이 정확한 시간을 가리키도록
